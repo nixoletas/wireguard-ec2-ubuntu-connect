4f90a5569a9872cf58d6c707f3bb1fae4d02f71a
Nicholas_Miyasato <66659340+nixoletas@users.noreply.github.com>
2024-09-24 00:55:22 -0400

    Update new-ubuntu.sh

--- new-ubuntu.sh
+++ new-ubuntu.sh
@@ -1,24 +1,20 @@
 sudo apt install -y wireguard
 
-# Cria o arquivo de configuração do WireGuard
 WG_CONFIG="/etc/wireguard/wg0.conf"
 
-# Verifica se o arquivo peer.conf existe
 if [[ -f "./peer.conf" ]]; then
-    # Copia o conteúdo do arquivo peer.conf para wg0.conf
     sudo cp ./peer.conf $WG_CONFIG
 else
-    echo "O arquivo peer.conf não foi encontrado. Certifique-se de que ele está no diretório atual."
+    echo "peer.conf file not found. Make sure it is placed on the current directory."
     exit 1
 fi
 
-# Configura permissões para o arquivo
 sudo chmod 600 $WG_CONFIG
 
-# Ativa a interface WireGuard
+# Activate interface
 sudo wg-quick up wg0
 
-# Habilita o WireGuard para iniciar automaticamente na inicialização
+# autostart on startup
 sudo systemctl enable wg-quick@wg0
 
-echo "WireGuard instalado e configurado com sucesso!"
+echo "Wireguard installed and successfully configured!"
